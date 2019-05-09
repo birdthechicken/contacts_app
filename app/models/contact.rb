b1d967c9bd5a478656277d77f3c78709e9b2718a
birdthechicken <sinangundogar61@gmail.com>
2019-05-09 16:50:23 -0500

    week3 day4

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,7 +3,7 @@ class Contact < ApplicationRecord
   validates :last_name, presence: true
  
   validates :email, uniqueness: true
-  validates :email, format: { with: /\A@\z/}
+  validates :email, format: { with: /\A\w+[@]\w{3,}[.]\w{2,}\z/, message: "must be valid email format" }
 
 
 
